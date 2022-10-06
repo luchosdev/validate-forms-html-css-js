0599014f13d573de78dd4c0273a42e55f6c09490
Luis Sequera <luissequeracontreras@gmail.com>
2022-10-06 17:58:35 -0500

    [ADD] code js to validate every input

--- js/main.js
+++ js/main.js
@@ -6,23 +6,51 @@ const password = document.querySelector('#passInput');
 const firstNameError = document.querySelector('#firstNameError');
 const lastNameError = document.querySelector('#lastNameError');
 const emailAddressError = document.querySelector('#emailError');
-const passwordError = document.querySelector('#passError');
+const passError = document.querySelector('#passError');
 
 const button = document.querySelector('#button');
 
 button.addEventListener('click', (event)=>{
     event.preventDefault();
-    validateEmpty(firstName.value, firstName);
+    validateEmpty(firstName.value, firstName, firstNameError, 'First Name cannot be empty');
+    validateEmpty(lastName.value, lastName, lastNameError, 'Last Name cannot be empty');
+    validatePassword(password.value, password, passError);
+    validateEmail(emailAddress.value, emailAddress, emailAddressError);
 });
 
-function validateEmpty(valueInput, divInput){
-    if(valueInput.length == 0){
-        showError(divInput);
-    }else {
-        hideError();
+function validateEmail(valueInput, divInput, divError) {
+    let regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    if(regExp.test(valueInput) == true){
+        hideError(divInput, divError);
+    } else {
+        showError(divInput, divError, 'Looks like this is not an email');
     }
 }
 
-function showError(divInput){
+function validatePassword(valueInput, divInput, divError) {
+    let regExp = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,64})/g;
+    if(regExp.test(valueInput) == true){
+        hideError(divInput, divError);
+    } else {
+        showError(divInput, divError, 'This is not a valid Password');
+    }
+}
+
+function validateEmpty(valueInput, divInput, divError, nameInput){
+    if(valueInput.length == 0){
+        showError(divInput, divError, nameInput);
+    }else {
+        hideError(divInput, divError);
+    }
+}
+
+function showError(divInput, divError, error){
     divInput.style.border = '1px solid red';
+    divError.innerHTML = `<img class="icon-error" src="./images/icon-error.svg" alt="">
+    <p class="error">${error}</p>`;
+}
+
+function hideError(divInput, divError){
+    divInput.style.border = '1px solid hsl(246, 25%, 77%)';
+    divError.innerHTML = ``;
 }
